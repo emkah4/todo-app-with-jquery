1174051fecbd1d5e0dbbe275d2689543a57a0521
emkah4 <sherys.emilis@gmail.com>
2020-10-24 15:58:21 +0300

    omayuva

--- app/jquery/localStorage.js
+++ app/jquery/localStorage.js
@@ -1,23 +1,63 @@
 let memory = [];
 
 $("svg").on("click", function(){
-    var text = $("input").value;
+    var text = $("input[type='text']").val();
     if (text) {
         storageAdd(text);
+    }
+    $("input[type='text']").val("");
+});
+
+$("input[type='text']").on("keypress", function(e){
+    if(e.which === 13) {
+        var text = $(this).val();
+        if (text) {
+            storageAdd(text);
+        }
+        $(this).val("");
     }
 })
 
 function storageAdd(text) {
     memory.push(text);
-    localStorage.setItem("elements", memory);
+    localStorage.setItem('todos', memory);
 }
 
-if (localStorage.getItem('elements')) {
-	memory = localStorage.getItem('elements').split(',');
+function storageRemove(index) {
+    for(let i = 0; i < memory.length; i++) {
+        if (i === index) {
+            memory.splice(i, 1);
+        }
+    }
+}
+
+// $("li").on("click", "img", function(event){
+//     event.stopPropagation();
+//     var target = $(this);
+//     var index = 0;
+
+//     while (target.prev()) {
+//         index++;
+//         target = target.prev();
+//         alert(index);
+//     }
+
+//     storageRemove(index);
+// })    ///////TESTI
+
+if (localStorage.getItem('todos')) {
+	memory = localStorage.getItem('todos').split(',');
 }
 
 if (memory.length) {
-	for (let i = 0; i < memory.length; i++) {
-        let text = memory[i];
-	}
+    for(let i = 0; i < memory.length; i++){
+        var text = memory[i];
+        memoryActions();
+    }
+}
+
+function memoryActions(){
+    $("ul").append("<li><img></img>" + text + "</li>");
+    $("img").attr("src", "cancel.png");
+
 }
--- app/jquery/script.js
+++ app/jquery/script.js
@@ -11,7 +11,6 @@ $("ul").on("click", "img", function(event){
 
 $("svg").on("click", function(){
     var todoText = $("input[type='text']").val();
-    $("input[type='text']").val("");
     $("ul").append("<li><img></img>" + todoText + "</li>")
     $("img").attr("src", "cancel.png")
 })
@@ -19,7 +18,6 @@ $("svg").on("click", function(){
 $("input[type='text']").keypress(function(event){
     if (event.which === 13) {
         var todoText = $(this).val();
-        $(this).val("");
         $("ul").append("<li><img></img>" + todoText + "</li>");
         $("img").attr("src", "cancel.png")
     }
